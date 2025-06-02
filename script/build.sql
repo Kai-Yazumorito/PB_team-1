f5e700ee921619d67cae533c5334367c1b1e9255
Kai-Yazumorito <agarciafranco03@gmail.com>
2025-06-01 19:48:49 -0600

    .

---
+++ script/build.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS " personal_blog ";
+USE "personal_blog";
+
+CREATE TABLE "bg01_blog" (
+    "id_blog" INT NOT NULL AUTO_INCREMENT,
+    "tx_title" VARCHAR(225) NOT NULL,
+    "md_content" TEXT NOT NULL,
+    "tm_createdAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "tm_updatedAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    "fk_id_category" INT NOT NULL
+);
