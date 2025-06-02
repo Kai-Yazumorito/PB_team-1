715e587aeb273a8020ea571f749bfd57a0707bdc
Kai-Yazumorito <agarciafranco03@gmail.com>
2025-06-01 19:51:35 -0600

    .

--- script/build.sql
+++ script/build.sql
@@ -1,8 +1,11 @@
 CREATE DATABASE IF NOT EXISTS " personal_blog ";
 USE "personal_blog";
 
+-- Datos sacados de la carpeta de 'diagram' del proyecto
+
 CREATE TABLE "bg01_blog" (
     "id_blog" INT NOT NULL AUTO_INCREMENT,
+    "title" VARCHAR(255) NOT NULL,
     "tx_title" VARCHAR(225) NOT NULL,
     "md_content" TEXT NOT NULL,
     "tm_createdAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
